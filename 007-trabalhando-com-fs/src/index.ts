489bf7c764f7665aa9dde6cee60c496306fa24a5
Edilson Segundo <edilsonsilvaa13@gmail.com>
2024-07-09 11:14:22 -0300

    Trabalhando com File System

--- 007-trabalhando-com-fs/src/index.ts
+++ 007-trabalhando-com-fs/src/index.ts
@@ -1,29 +1,47 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
+const caminhoArquivo = path.resolve('tmp', 'arquivo.txt');
+const content = `1 - Deu bom na criação do arquivo!\n2 - Deu bom na criação do arquivo!`;
+const meuTextoArquivo = fs.readFileSync(caminhoArquivo, 'utf8');
+
 try {
-    //verifica se existe essa pasta e criando-a caso não exista
+    //verifica se existe essa pasta e a cria caso não exista
    if(!fs.existsSync(path.resolve('tmp'))){
-    fs.mkdirSync(path.resolve('tmp'));
+        fs.mkdirSync(path.resolve('tmp'));
    }
    //verifica se existe esse arquivo e o cria caso não exista
-   const caminhoArquivo = path.resolve('tmp', 'arquivo.txt');
    if(!fs.existsSync(caminhoArquivo)){
-    const content = `1 - Deu bom na criação do arquivo!\n2 - Deu bom na criação do arquivo!`;
-    fs.writeFileSync(caminhoArquivo, content, 'utf8');
+        fs.writeFileSync(caminhoArquivo, content, 'utf8');
+    }    
+    //Adiciona novos conteúdos no arquivo criado.
+    fs.writeFileSync(caminhoArquivo, `${meuTextoArquivo}\nteste`);
+   //Verifica se existe, caso não exista, cria e renomeia a pasta "nova-pasta-01" para "nova-pasta-02"
+   if(!fs.existsSync(path.resolve('tmp', 'nova-pasta-02'))){
+        fs.mkdirSync(path.resolve('tmp', 'nova-pasta-01'));
+        fs.renameSync(path.resolve('tmp', 'nova-pasta-01'), path.resolve('tmp', 'nova-pasta-02'));
+        console.log("Pasta criada com sucesso!");
+   } else {
+        console.log("Pasta já criada!");
    }
-   //Adiciona novos conteúdos no arquivo criado.
-   const meuTextoArquivo = fs.readFileSync(caminhoArquivo, 'utf8');
-   fs.writeFileSync(caminhoArquivo, `${meuTextoArquivo}\n3 - teste`)
-   //apresenta no terminal o conteúdo do arquivo
-   console.log(
-    `Dados do arquivo:\n`,
-    fs.readFileSync(caminhoArquivo, 'utf8')
-   );
-   //Cria uma nova pasta dentro da pasta tmp
-   fs.mkdirSync(path.resolve('tmp', 'nova-pasta-01'));
-   //renomea o arquivo
+   //verifica se existe, caso não exista, cria e renomeia o arquivo "arquivo-01.txt" para "arquivo-02.txt"
+   if(!fs.existsSync(path.resolve('tmp', 'nova-pasta-02', 'arquivo-02.txt'))){
+        fs.writeFileSync(path.resolve('tmp', 'nova-pasta-02', 'arquivo-01.txt'), content);
+        fs.renameSync(path.resolve('tmp', 'nova-pasta-02', 'arquivo-01.txt'), path.resolve('tmp', 'nova-pasta-02', 'arquivo-02.txt'));
+        console.log("Arquivo criado com sucesso!");
+    } else {
+        console.log("Arquivo já criada!");
+    }
 
+    if(!fs.existsSync(path.resolve('tmp', 'nova-pasta-02', 'pode-deletar'))){
+        fs.mkdirSync(path.resolve('tmp', 'nova-pasta-02', 'pode-deletar'));
+   }
+
+    //remove a pasta nova-pasta-02 após 1 minuto de criada
+    setTimeout(() => fs.rmSync(path.resolve('tmp', 'nova-pasta-02', 'pode-deletar'),{ 
+        recursive: true
+    }), 6000);
+    
 } catch (error) {
     console.log(error);
 }
